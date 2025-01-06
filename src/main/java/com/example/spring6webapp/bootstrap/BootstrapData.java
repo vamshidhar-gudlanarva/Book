3f5ec5eeacbaef6782999cdb83a344beb3406034
vamshidhar <vamshidhar1001@gmail.com>
2025-01-05 20:17:11 -0800

    enabled the h2 console

--- src/main/java/com/example/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/com/example/spring6webapp/bootstrap/BootstrapData.java
@@ -49,6 +49,8 @@ public class BootstrapData implements CommandLineRunner {
 
         ericSaved.getBooks().add(dddSaved);
         robSaved.getBooks().add(noEJBSaved);
+        dddSaved.getAuthors().add(ericSaved);
+        noEJBSaved.getAuthors().add(robSaved);
 
         Publisher publisher = new Publisher();
         publisher.setPublisherName("vamshi");
